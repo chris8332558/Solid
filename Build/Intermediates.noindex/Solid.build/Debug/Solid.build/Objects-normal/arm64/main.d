478b0b8df83120b73c2c3a3f961d35d31cf0b3d2
chris8332558 <chris8332558@gmail.com>
2025-01-19 19:09:57 -0800

    Add SolidEngine and Sandbox

---
+++ Build/Intermediates.noindex/Solid.build/Debug/Solid.build/Objects-normal/arm64/main.d
@@ -0,0 +1,1 @@
+dependencies: /Users/chris/dev/Solid/Sandbox/main.cpp
---
+++ Sandbox/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Solid
+//
+//  Created by YING JIEH XIA on 2025/1/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
