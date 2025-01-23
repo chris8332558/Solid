e850c14eba3a6c4aa056c02afa94f7a32ddaea93
chris8332558 <chris8332558@gmail.com>
2025-01-22 21:49:48 -0800

    Move Solid and create Sandbox folders

---
+++ Solid/build/examples/CMakeFiles/SolidEngineExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SolidEngineExample.dir/main.cpp.o"
+  "CMakeFiles/SolidEngineExample.dir/main.cpp.o.d"
+  "SolidEngineExample"
+  "SolidEngineExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SolidEngineExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Solid/build/examples/CMakeFiles/SolidEngineExample.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+examples/CMakeFiles/SolidEngineExample.dir/main.cpp.o: \
+  /Users/chris/dev/Solid/Solid/examples/main.cpp \
+  /Users/chris/dev/Solid/Solid/include/SolidEngine.hpp
